a07c002bf45042cdf3466e9251e82a05da94f3b1
Dima Lyamov <dimalyamov@yandex.ru>
2017-03-27 13:02:55 +0300

    new text predictions, modified predictions according to zodiac sign on a monthly

--- app/src/main/java/com/example/horoscope/Main2Activity.java
+++ app/src/main/java/com/example/horoscope/Main2Activity.java
@@ -1,5 +1,6 @@
 package com.example.horoscope;
 
+import android.content.res.Resources;
 import android.icu.util.Calendar;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
@@ -14,14 +15,23 @@ import java.util.Random;
 
 public class Main2Activity extends AppCompatActivity {
 
-    ImageView iv1, iv2, iv3, iv4, iv5;
-    TextView tv1, tv2, tv3, tv4, tv5, topTW;
 
-    int s;
+    ImageView [] IVmass = new ImageView[5];
+    TextView [] TVmass = new TextView[5];
+
+
+    Random [] randoms = new Random[5];
+    int [] randomResults = new int[5];
+
+    TextView topTW;
+
+    int userBirthday , sumOfCurrentDay;
     String top;
+
 
     int images[] = {R.drawable.pk1, R.drawable.pk2, R.drawable.pk3,
             R.drawable.pk4, R.drawable.pk5, R.drawable.pk6};
+
 
     @RequiresApi(api = Build.VERSION_CODES.N)
 
@@ -29,86 +39,50 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        s = getIntent().getExtras().getInt("RESULT");
+        userBirthday = getIntent().getExtras().getInt("RESULT");
         top = getIntent().getExtras().getString("TOP");
 
         topTW = (TextView) findViewById(R.id.topTextView);
         topTW.setText(top);
 
 
+        IVmass[0] = (ImageView) findViewById(R.id.hpIV);
+        IVmass[1] = (ImageView) findViewById(R.id.luckIV);
+        IVmass[2] = (ImageView) findViewById(R.id.moneyIV);
+        IVmass[3] = (ImageView) findViewById(R.id.workIV);
+        IVmass[4] = (ImageView) findViewById(R.id.loveIV);
 
-        iv1 = (ImageView) findViewById(R.id.imageViev1);
-        iv2 = (ImageView) findViewById(R.id.imageViev2);
-        iv3 = (ImageView) findViewById(R.id.imageViev3);
-        iv4 = (ImageView) findViewById(R.id.imageViev4);
-        iv5 = (ImageView) findViewById(R.id.imageViev5);
 
-
-        tv1 = (TextView) findViewById(R.id.hpTextView);
-        tv2 = (TextView) findViewById(R.id.luckTextView);
-        tv3 = (TextView) findViewById(R.id.moneyTextView);
-        tv4 = (TextView) findViewById(R.id.workTextView);
-        tv5 = (TextView) findViewById(R.id.loveTextView);
+        TVmass[0] = (TextView) findViewById(R.id.hpTextView);
+        TVmass[1] = (TextView) findViewById(R.id.luckTextView);
+        TVmass[2] = (TextView) findViewById(R.id.moneyTextView);
+        TVmass[3] = (TextView) findViewById(R.id.workTextView);
+        TVmass[4] = (TextView) findViewById(R.id.loveTextView);
 
         Calendar c = Calendar.getInstance();
 
-        int day = c.get(Calendar.DAY_OF_MONTH);
-        int month = c.get(Calendar.MONTH);
-        int year = c.get(Calendar.YEAR);
+        if(getIntent().getExtras().getBoolean("FLAG")){
+            sumOfCurrentDay = c.get(Calendar.DAY_OF_MONTH);
+        }
 
+        sumOfCurrentDay += c.get(Calendar.MONTH) + c.get(Calendar.YEAR);
 
+        for(int i = 0; i < 5; i++){
+            randoms[i] = new Random(userBirthday+sumOfCurrentDay+i+1);
+            randomResults [i] = randoms[i].nextInt(6);
+            IVmass[i].setImageResource(images[randomResults [i]]);
 
-        int seed1 = s+day+month+year+1;
-        int seed2 = s+day+month+year+2;
-        int seed3 = s+day+month+year+3;
-        int seed4 = s+day+month+year+4;
-        int seed5 = s+day+month+year+5;
+        }
 
+        Resources res = getResources();
 
-
-        Random r1 = new Random(seed1);
-        Random r2 = new Random(seed2);
-        Random r3 = new Random(seed3);
-        Random r4 = new Random(seed4);
-        Random r5 = new Random(seed5);
-
-
-        int randomResultHp = r1.nextInt(6);
-        int randomResultLuck = r2.nextInt(6);
-        int randomResultMoney = r3.nextInt(6);
-        int randomResultWork = r4.nextInt(6);
-        int randomResultLove = r5.nextInt(6);
-
-        //textView.setText(R.string.hello);
-
-        if(randomResultHp<2)
-            tv1.setText(R.string.badHp);
-        if (randomResultHp>3)
-            tv1.setText(R.string.goodHp);
-        if(randomResultLuck<2)
-            tv2.setText(R.string.badLuck);
-        if (randomResultLuck>3)
-            tv2.setText(R.string.goodLuck);
-        if(randomResultMoney<2)
-            tv3.setText(R.string.badMoney);
-        if (randomResultMoney>3)
-            tv3.setText(R.string.goodMoney);
-        if(randomResultWork<2)
-            tv4.setText(R.string.badWork);
-        if (randomResultWork>3)
-            tv4.setText(R.string.goodWork);
-        if(randomResultLove<2)
-            tv5.setText(R.string.badLove);
-        if (randomResultLove>3)
-            tv5.setText(R.string.goodLove);
-
-
-        iv1.setImageResource(images[randomResultHp]);
-        iv2.setImageResource(images[randomResultLuck]);
-        iv3.setImageResource(images[randomResultMoney]);
-        iv4.setImageResource(images[randomResultWork]);
-        iv5.setImageResource(images[randomResultLove]);
-
+        TVmass[0].setText(res.getStringArray(R.array.textPredictionHealth)[randomResults [0]]);
+        TVmass[1].setText(res.getStringArray(R.array.textPredictionLuck)[randomResults [1]]);
+        TVmass[2].setText(res.getStringArray(R.array.textPredictionMony)[randomResults [2]]);
+        TVmass[3].setText(res.getStringArray(R.array.textPredictionWork)[randomResults [3]]);
+        TVmass[4].setText(res.getStringArray(R.array.textPredictionLove)[randomResults [4]]);
 
     }
+
+
 }
--- app/src/main/java/com/example/horoscope/MainActivity.java
+++ app/src/main/java/com/example/horoscope/MainActivity.java
@@ -16,10 +16,9 @@ public class MainActivity extends AppCompatActivity {
     int day,month,year;
     EditText numberOfDay, numberOfMonth, numberOfYear;
 
-    //int[] daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
     private static  final String RESULT = "RESULT";
     private static  final String TOP = "TOP";
+    private static  final String FLAG = "FLAG";
 
     String birthdayStr;
 
@@ -47,10 +46,9 @@ public class MainActivity extends AppCompatActivity {
                     birthdayStr = ""+year+"."+month+"."+day;
                     intent.putExtra(RESULT,result);
                     intent.putExtra(TOP, birthdayStr);
+                    intent.putExtra(FLAG, true);
+
                     startActivity(intent);
-
-
-
             }
         });
 
@@ -59,55 +57,56 @@ public class MainActivity extends AppCompatActivity {
 
     public void newScreen(View v) {
         Intent intObj = new Intent(this, Main2Activity.class);
+        intObj.putExtra(FLAG, false);
 
         switch(v.getId()) {
             case R.id.button6:
-                intObj.putExtra("RESULT", 12);
-                intObj.putExtra("TOP", "Рыбы");
+                intObj.putExtra(RESULT, 12);
+                intObj.putExtra(TOP, "Рыбы");
                 break;
             case R.id.button5:
-                intObj.putExtra("RESULT", 11);
-                intObj.putExtra("TOP", "Рак");
+                intObj.putExtra(RESULT, 11);
+                intObj.putExtra(TOP, "Рак");
                 break;
             case R.id.button4:
-                intObj.putExtra("RESULT", 10);
-                intObj.putExtra("TOP", "Водолей");
+                intObj.putExtra(RESULT, 10);
+                intObj.putExtra(TOP, "Водолей");
                 break;
             case R.id.button3:
-                intObj.putExtra("RESULT", 9);
-                intObj.putExtra("TOP", "Весы");
+                intObj.putExtra(RESULT, 9);
+                intObj.putExtra(TOP, "Весы");
                 break;
             case R.id.button10:
-                intObj.putExtra("RESULT", 8);
-                intObj.putExtra("TOP", "Телец");
+                intObj.putExtra(RESULT, 8);
+                intObj.putExtra(TOP, "Телец");
                 break;
             case R.id.button9:
-                intObj.putExtra("RESULT", 7);
-                intObj.putExtra("TOP", "Овен");
+                intObj.putExtra(RESULT, 7);
+                intObj.putExtra(TOP, "Овен");
                 break;
             case R.id.button8:
-                intObj.putExtra("RESULT", 6);
-                intObj.putExtra("TOP", "Козерог");
+                intObj.putExtra(RESULT, 6);
+                intObj.putExtra(TOP, "Козерог");
                 break;
             case R.id.button7:
-                intObj.putExtra("RESULT", 5);
-                intObj.putExtra("TOP", "Лев");
+                intObj.putExtra(RESULT, 5);
+                intObj.putExtra(TOP, "Лев");
                 break;
             case R.id.button12:
-                intObj.putExtra("RESULT", 4);
-                intObj.putExtra("TOP", "Близнецы");
+                intObj.putExtra(RESULT, 4);
+                intObj.putExtra(TOP, "Близнецы");
                 break;
             case R.id.button14:
-                intObj.putExtra("RESULT", 3);
-                intObj.putExtra("TOP", "Дева");
+                intObj.putExtra(RESULT, 3);
+                intObj.putExtra(TOP, "Дева");
                 break;
             case R.id.button13:
-                intObj.putExtra("RESULT", 2);
-                intObj.putExtra("TOP", "Скорпион");
+                intObj.putExtra(RESULT, 2);
+                intObj.putExtra(TOP, "Скорпион");
                 break;
             case R.id.button11:
-                intObj.putExtra("RESULT", 1);
-                intObj.putExtra("TOP", "Стрелец");
+                intObj.putExtra(RESULT, 1);
+                intObj.putExtra(TOP, "Стрелец");
                 break;
         }
 
